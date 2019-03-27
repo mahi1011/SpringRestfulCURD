d7fb79a50a22566bee40edb0e9d5ac45da9dc3af
Galib <Galib@Galib-PC>
2019-03-28 01:05:03 +0530

    First Commit

---
+++ SpringRestCurd/src/main/java/com/mahesh/spring/dao/DataDao.java
@@ -0,0 +1,15 @@
+package com.mahesh.spring.dao;
+
+import java.util.List;
+
+import com.mahesh.spring.model.Employee;
+
+public interface DataDao {
+
+	public boolean addEntity(Employee employee) throws Exception;
+	public Employee getEntityById(long id) throws Exception;
+	public List<Employee> getEntityList() throws Exception;
+	public boolean deleteEntity(long id) throws Exception;
+
+	
+}
